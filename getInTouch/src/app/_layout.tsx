f17c0f7417e0809e5d120c00b7b21d57d598601d
Viliam Rideky <rideky.viliam@gmail.com>
2026-05-16 23:37:09 +0200

    auth logic init

--- getInTouch/src/app/_layout.tsx
+++ getInTouch/src/app/_layout.tsx
@@ -1,13 +1,25 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const router = useRouter();
+
+  let isAuth = false;
+
+  useEffect(() => {
+    if (!isAuth) {
+      router.replace("/(auth)/login");
+    }
+  });
+
   return (
     <Stack
       screenOptions={{
         headerShown: false,
       }}
     >
-      <Stack.Screen name="index" />
+      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="(auth)" />
     </Stack>
   );
 }
