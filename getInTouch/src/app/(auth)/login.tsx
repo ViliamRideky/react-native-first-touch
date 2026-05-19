66d46c9c7472c7f7f18c8aa0879bc441eca23644
Viliam Rideky <rideky.viliam@gmail.com>
2026-05-19 19:20:35 +0200

    created login and singup screens with router simple functionality

--- getInTouch/src/app/(auth)/login.tsx
+++ getInTouch/src/app/(auth)/login.tsx
@@ -1,9 +1,109 @@
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import {
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function Login() {
+export default function LoginScreen() {
+  const router = useRouter();
+
+  const navigateToSignUp = () => {
+    router.push("/(auth)/singup");
+  };
+
   return (
-    <View>
-      <Text>Login Screen</Text>
-    </View>
+    <SafeAreaView edges={["top", "bottom"]} style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.title}>Welcome back</Text>
+        <Text style={styles.subtitle}>Sign In to Continue</Text>
+        <View style={styles.form}>
+          <TextInput
+            placeholder="Email..."
+            placeholderTextColor={"#999"}
+            keyboardType="email-address"
+            autoComplete="email"
+            style={styles.input}
+          />
+          <TextInput
+            placeholder="Password..."
+            placeholderTextColor={"#999"}
+            autoComplete="password"
+            secureTextEntry
+            autoCapitalize="none"
+            style={styles.input}
+          />
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Sign In</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.linkButton} onPress={navigateToSignUp}>
+          <Text style={styles.linkButtonText}>
+            Don't have an account?{" "}
+            <Text style={styles.linkButtonTextBold}>Sing up</Text>
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 24,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    marginBottom: 8,
+    color: "#027a00",
+  },
+  subtitle: {
+    fontSize: 16,
+    marginBottom: 32,
+    color: "#666",
+  },
+  form: {
+    width: "100%",
+  },
+  input: {
+    backgroundColor: "#F5F5F5",
+    borderRadius: 12,
+    padding: 16,
+    fontSize: 16,
+    marginBottom: 16,
+    borderWidth: 1,
+    borderColor: "#e0e0e0",
+  },
+  button: {
+    backgroundColor: "#027a00",
+    borderRadius: 12,
+    padding: 16,
+    alignItems: "center",
+    marginBottom: 12,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+  linkButton: {
+    alignItems: "center",
+  },
+  linkButtonText: {
+    color: "#666",
+    fontSize: 16,
+  },
+  linkButtonTextBold: {
+    fontWeight: "600",
+    color: "#000",
+  },
+});
